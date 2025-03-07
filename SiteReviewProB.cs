e1fd91bfa6f043d669c067316f6e71926c168856
Adi Makkar <adimakkar.ca@gmail.com>
2025-03-07 11:15:28 -0500

    wip - site review script for pro b

---
+++ SiteReviewProB.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Azure.WebJobs;
+using PnP.Framework.Modernization.Functions;
+using System;
+using System.Threading.Tasks;
+
+namespace SiteReviewProB
+{
+	public static class SiteReviewProB 
+	{
+		[FunctionName("SiteReviewProB")]
+		public static async Task RunProB(
+            [TimerTrigger("0 0 * * * *")] TimerInfo myTimer, ILogger log, ExecutionContext executionContext)
+			//[HttpTrigger(AuthorizationLevel.Anonymous, "get")] HttpRequest req, ILogger log, ExecutionContext executionContext)
+		{
+            log.LogInformation($"SiteReviewProB timer trigger function executed at: {DateTime.Now}");
+        }
+		)
+	}
+}
